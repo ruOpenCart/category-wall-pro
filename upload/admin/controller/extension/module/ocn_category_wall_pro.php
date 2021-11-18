4f9b4b506836cd12a19a860dd40880e20dd00be5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2021-11-18 21:56:08 +0300

    Исправление мелких ошибок

--- upload/admin/controller/extension/module/ocn_category_wall_pro.php
+++ upload/admin/controller/extension/module/ocn_category_wall_pro.php
@@ -12,9 +12,6 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 
 	private $error = [];
 
-	public function install() {
-	}
-
 	public function index() {
 		$this->load->language('extension/module/ocn_category_wall_pro');
 
@@ -23,22 +20,25 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		$this->load->model('setting/module');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-			
-			if (!isset($this->request->get['module_id'])) {
+			$module_id = isset($this->request->get['module_id']) ? $this->request->get['module_id'] : 0;
+			if (!$module_id) {
 				$this->model_setting_module->addModule('ocn_category_wall_pro', $this->request->post);
+				$modules = $this->model_setting_module->getModulesByCode('ocn_category_wall_pro');
+				$last = count($modules) - 1;
+				$module_id = $modules[$last]['module_id'];
 			} else {
-				$this->model_setting_module->editModule($this->request->get['module_id'], $this->request->post);
-			}		
+				$this->model_setting_module->editModule($module_id, $this->request->post);
+			}
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
 			// If button apply
 			if (isset($this->request->post['apply']) && $this->request->post['apply']) {
-				$this->response->redirect($this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true));
+				$this->response->redirect($this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $module_id, true));
 			}
 
 			// Go to list modules
-			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'], true));
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true));
 		}
 
 		$this->getForm();
@@ -57,10 +57,10 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 			'module_ocn_category_wall_pro_description_status' => 1,
 			'module_ocn_category_wall_pro_description_length' => 30,
 			'module_ocn_category_wall_pro_categories_type' => 'all',
-			'module_ocn_category_wall_pro_categories_selected' => array(),
-			'module_ocn_category_wall_pro_categories_custom' => array()
+			'module_ocn_category_wall_pro_categories_selected' => [],
+			'module_ocn_category_wall_pro_categories_custom' => []
 		];
-			
+
 		$data['data_current_version'] = $this->extension_version;
 		$data['data_author'] = $this->author;
 		$data['data_author_repository'] = $this->author_repository;
@@ -72,9 +72,9 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		$data['url_extension_update'] = $this->url_extension_update;
 
 		$data['user_token'] = $this->session->data['user_token'];
+		$module_id = isset($this->request->get['module_id']) ? $this->request->get['module_id'] : 0;
 
 		//Errors
-	
 		if (isset($this->session->data['success'])) {
 			$data['success'] = $this->session->data['success'];
 			unset($this->session->data['success']);
@@ -82,13 +82,8 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 			$data['success'] = '';
 		}
 
-		if (isset($this->error['name'])) {
-			$data['error_name'] = $this->error['name'];
-		} else {
-			$data['error_name'] = '';
-		}
-				
 		$data['error_warning'] = isset($this->error['warning']) ? $this->error['warning'] : [];
+		$data['error_name'] = isset($this->error['name']) ? $this->error['name'] : [];
 		$data['error_title'] = isset($this->error['title']) ? $this->error['title'] : [];
 		$data['error_width'] = isset($this->error['width']) ? $this->error['width'] : [];
 		$data['error_height'] = isset($this->error['height']) ? $this->error['height'] : [];
@@ -109,23 +104,22 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		];
 		$data['breadcrumbs'][] = [
 			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'], true)
+			'href' => $this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'] . ($module_id ? '&module_id=' . $module_id : ''), true)
 		];
 
-		// Buttons		
-		if (!isset($this->request->get['module_id'])) {
+		// Buttons
+		if (!isset($module_id)) {
 			$data['url_action'] = $this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'], true);
 		} else {
-			$data['url_action'] = $this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $this->request->get['module_id'], true);
-		}		
+			$data['url_action'] = $this->url->link('extension/module/ocn_category_wall_pro', 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $module_id, true);
+		}
 		$data['url_cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
-		if (isset($this->request->get['module_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
-			$module_info = $this->model_setting_module->getModule($this->request->get['module_id']);
+		if (isset($module_id) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+			$module_info = $this->model_setting_module->getModule($module_id);
 		}
 
 		// Data form
-		
 		if (isset($this->request->post['name'])) {
 			$data['name'] = $this->request->post['name'];
 		} elseif (!empty($module_info)) {
@@ -133,10 +127,9 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		} else {
 			$data['name'] = '';
 		}
-				
+
 		$this->load->model('localisation/language');
 		$data['languages'] = $this->model_localisation_language->getLanguages();
-
 		foreach ($data['languages'] as $language) {
 			if (isset($this->request->post['module_ocn_category_wall_pro_title'][$language['language_id']]['title'])) {
 				$data['module_ocn_category_wall_pro_title'][$language['language_id']]['title'] = $this->request->post['module_ocn_category_wall_pro_title'][$language['language_id']]['title'];
@@ -161,7 +154,7 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		} else {
 			$data['module_ocn_category_wall_pro_categories_custom'] = $default_data['module_ocn_category_wall_pro_categories_custom'];
 		}
-		
+
 		$data['status'] = isset($this->request->post['status'])
 			? $this->request->post['status']
 			: (!empty($module_info) ? $module_info['status'] : $default_data['module_ocn_category_wall_pro_status']);
@@ -226,18 +219,17 @@ class ControllerExtensionModuleOCNCategoryWallPro extends Controller {
 		if (!$this->user->hasPermission('modify', 'extension/module/ocn_category_wall_pro')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
-		
-		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
-			$this->error['name'] = $this->language->get('error_name');
-		}
 
 		$this->load->model('localisation/language');
 		$data['languages'] = $this->model_localisation_language->getLanguages();
-
 		foreach ($data['languages'] as $language) {
 			if ((utf8_strlen($this->request->post['module_ocn_category_wall_pro_title'][$language['language_id']]['title']) < 1) || (utf8_strlen($this->request->post['module_ocn_category_wall_pro_title'][$language['language_id']]['title']) > 255)) {
 				$this->error['title'][$language['language_id']] = $this->language->get('error_title');
 			}
+		}
+
+		if ((utf8_strlen($this->request->post['name']) < 3) || (utf8_strlen($this->request->post['name']) > 64)) {
+			$this->error['name'] = $this->language->get('error_name');
 		}
 
 		if (!$this->request->post['module_ocn_category_wall_pro_image_width']) {
